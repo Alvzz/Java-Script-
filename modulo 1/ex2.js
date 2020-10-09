57821bcb6e19c3ae4161e899f589b88f09f92048
João Vitor Alves <72218359+Alvzz@users.noreply.github.com>
2020-10-09 02:41:31 -0300

    Add files via upload

---
+++ modulo 1/ex2.js
@@ -0,0 +1,8 @@
+const pares = (a, b) => {
+    for (var i = a; i <= b; i++){
+        if ( i % 2 == 0){
+            console.log(i);  
+        }       
+    }       
+};       
+pares(32, 321);
---
+++ modulo 1/ex3.js
@@ -0,0 +1,5 @@
+var skills = ["Javascript", "ReactJS", "React Native"];
+    function temHabilidade(skills) {
+        return skills.indexOf("Javascript") !== -1;
+    }
+    console.log(temHabilidade(skills));
